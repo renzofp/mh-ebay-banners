80a8ed0fec1cac82b84afc195e9f92bd74cd2c3e
Renzo F <renzofernandini@gmail.com>
2021-04-18 12:52:37 -0400

    add community v2 160x600

--- Community_V2/Ebay_Community_V2_160x600_EN/assets/js/custom.js
+++ Community_V2/Ebay_Community_V2_160x600_EN/assets/js/custom.js
@@ -65,19 +65,19 @@ function createTimeline(){
         tl.to(frame1_text, {duration:0.6, y:0, force3D:"true", ease: "power4.out"}, "frame2+=0.75");
 
         tl.to([frame1_cta, frame1_ctaArrow], {duration:0.8, stagger:0.1, autoAlpha:1, x:0, force3D:"true", ease: "power3.out"}, "frame2+=1.1");
-        tl.to(logoTop, {fill: "#07522c" , ease: "power4.out"}, "frame2");
+        tl.to(logoTop, {fill: "#592e13" , ease: "power4.out"}, "frame2");
 
 
         tl.addLabel("frame3", "+=2.2");
-        tl.to(frame1_ctaArrow, {duration:0.2, scaleX:0.2, autoAlpha:0, x:78, force3D:"true", ease: "power4.out"}, "frame3");
+        tl.to(frame1_ctaArrow, {duration:0.2, scaleX:0.2, autoAlpha:0, x:30, force3D:"true", ease: "power4.out"}, "frame3");
         tl.to(frame1_ctaArrow, {duration:0.5, scaleX:1, autoAlpha:1, x:0, force3D:"true", ease: "power4.out"}, "frame3+=0.4");
         
         tl.addLabel("frame4", "+=2.2");
-        tl.to(frame1_ctaArrow, {duration:0.2, scaleX:0.2, autoAlpha:0, x:78, force3D:"true", ease: "power4.out"}, "frame4");
+        tl.to(frame1_ctaArrow, {duration:0.2, scaleX:0.2, autoAlpha:0, x:30, force3D:"true", ease: "power4.out"}, "frame4");
         tl.to(frame1_ctaArrow, {duration:0.5, scaleX:1, autoAlpha:1, x:0, force3D:"true", ease: "power4.out"}, "frame4+=0.4");
         
         tl.addLabel("frame5", "+=2.2");
-        tl.to(frame1_ctaArrow, {duration:0.2, scaleX:0.2, autoAlpha:0, x:78, force3D:"true", ease: "power4.out"}, "frame5");
+        tl.to(frame1_ctaArrow, {duration:0.2, scaleX:0.2, autoAlpha:0, x:30, force3D:"true", ease: "power4.out"}, "frame5");
         tl.to(frame1_ctaArrow, {duration:0.5, scaleX:1, autoAlpha:1, x:0, force3D:"true", ease: "power4.out"}, "frame5+=0.4");
         
     /* ***** End Coding Here ***** */
